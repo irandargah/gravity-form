9d5b89c039240eeb4cd4892b2cbc632fa65fd746
Mohammad Saadat <mohammad2682593@gmail.com>
2020-11-23 14:25:09 +0330

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: درگاه پرداخت ایران درگاه
+Description: افزونه درگاه پرداخت ایران درگاه برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 1.0
+*/
+if (!defined('ABSPATH')) exit;
+require_once('irandargah.php');
